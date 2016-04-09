70e012357735cb257c2ffb52baf42cb0b8f94c5e
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-09 17:53:12 -0400

    ASM ISR handlers

--- src/boot.asm
+++ src/boot.asm
@@ -63,4 +63,81 @@ codesegment:
 loadIDT:
     mov eax, [esp + 4]
     lidt [eax]
-    return
+    ret
+
+; ISR stub macros
+%macro isr_code 1
+    isr%1:
+        cli
+        push byte %1
+        jmp isrHandlerInterm
+%endmacro
+
+%macro isr_stub 1
+    isr%1:
+        cli
+        push byte 42
+        push byte %1
+        jmp isrHandlerInterm
+%endmacro
+
+; exception stubs
+isr_stub 0
+isr_stub 1
+isr_stub 2
+isr_stub 3
+isr_stub 4
+isr_stub 5
+isr_stub 6
+isr_stub 7
+isr_code 8
+isr_stub 9
+isr_code 10
+isr_code 11
+isr_code 12
+isr_code 13
+isr_code 14
+isr_stub 15
+isr_stub 16
+isr_stub 17
+isr_stub 18
+isr_stub 19
+isr_stub 20
+isr_stub 21
+isr_stub 22
+isr_stub 23
+isr_stub 24
+isr_stub 25
+isr_stub 26
+isr_stub 27
+isr_stub 28
+isr_stub 29
+isr_stub 30
+isr_stub 31
+
+; general handler
+extern isrHandler
+isrHandlerInterm:
+   pusha
+
+   mov ax, ds
+   push ax
+
+   mov ax, 0x10
+   mov ds, ax
+   mov es, ax
+   mov fs, ax
+   mov gs, ax
+
+   call isrHandler
+
+   pop ax
+   mov ds, ax
+   mov es, ax
+   mov fs, ax
+   mov gs, ax
+
+   popa
+   add esp, 8
+   sti
+   iret
