83980033245f97639660f9c45fca3eea4869e2b6
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-09 17:31:00 -0400

    Begin IDT

--- src/kidt.c
+++ src/kidt.c
@@ -2,4 +2,60 @@
  * Interrupt descriptor table (C half)
  */
 
+#include <kidt.h>
 
+uint8_t idtTable[8 * 3];
+
+void initIDT() {
+    idtEntry(&idtTable, 0, isr0, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr1, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr2, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr3, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr4, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr5, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr6, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr7, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr8, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr9, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr10, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr11, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr12, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr13, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr14, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr15, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr16, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr17, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr18, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr19, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr20, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr21, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr22, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr23, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr24, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr25, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr26, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr27, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr28, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr29, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr30, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+    idtEntry(&idtTable, 0, isr31, 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
+}
+
+void idtEntry(
+        void* table,
+        int number,
+        void* offset,
+        uint16_t selector,
+        uint8_t flags) {
+    uint8_t* entry = table + (number * 8);
+    uint32_t _offset = (uint32_t) offset;
+
+    entry[0] = _offset & 0x00FF;
+    entry[1] = (_offset & 0xFF00) >> 8;
+    entry[2] = selector & 0x00FF;
+    entry[3] = (selector & 0xFF00) >> 8;
+    entry[4] = 0;
+    entry[5] = flags;
+    entry[6] = (_offset >> 16) & 0x00FF;
+    entry[7] = ((_offset >> 16) & 0xFF00) >> 8;
+}
--- src/kidt.h
+++ src/kidt.h
@@ -5,5 +5,50 @@
 #include <kdescriptor.h>
 
 void initIDT();
+void idtEntry(void* table, int number, void* offset, uint16_t selector, uint8_t flags);
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+#define IDT_GATE_INTERRUPT (0x6)
+#define IDT_GATE_TRAP (0xF)
+#define IDT_GATE_TASK (0x5)
+
+#define IDT_GATE_PRESENT (1<<7)
+
+#define IDT_MIN_RING0 (0)
+#define IDT_MIN_RING1 (1<<5)
+#define IDT_MIN_RING2 (1<<6)
+#define IDT_MIN_RING3 ((1<<5)|(1<<6))
 
 #endif
