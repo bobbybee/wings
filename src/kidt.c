3462754678a4b53b28c83d6fdaff2a4cbbbbf83a
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-14 17:49:23 -0400

    Somehow it works <3

--- src/kidt.c
+++ src/kidt.c
@@ -10,22 +10,24 @@ IDTEntry idtTable[48];
 
 void idtEntry(int number, void* offset, uint16_t selector, uint8_t flags);
 
-void* idtOffsets = {
+void* idtOffsets[] = {
     isr0, isr1, isr2, isr3, isr4, isr5, isr6, isr7, isr8, isr9, isr10, isr11,
     isr12, isr13, isr14, isr15, isr16, isr17, isr18, isr19, isr20, isr21, isr22,
     isr23, isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31,
 
-    irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7
+    irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7,
     irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15
 };
 
 
 void initIDT() {
+    kputs("IDT initialization...");
+
     for(int gate = 0; gate < 32; ++gate) {
         idtEntry(gate, idtOffsets[gate], 0x8, IDT_GATE_PRESENT | IDT_GATE_TRAP | IDT_MIN_RING0);
     }
 
-    for(gate = 32; gate < 48; ++gate) {
+    for(int gate = 32; gate < 48; ++gate) {
         idtEntry(gate, idtOffsets[gate], 0x8, IDT_GATE_PRESENT | IDT_GATE_INTERRUPT | IDT_MIN_RING0);
     }
 
@@ -34,7 +36,7 @@ void initIDT() {
     ptr.offset = &idtTable;
     loadIDT(&ptr);
 
-    kputs("Initialized\n");
+    kputs("IDT Initialized\n");
 }
 
 void idtEntry(
