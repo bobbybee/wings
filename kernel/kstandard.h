01514a8b644d85a80abaa8085a7cd9c8c1d27a31
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-23 13:01:29 -0400

    License

--- kernel/kstandard.h
+++ kernel/kstandard.h
@@ -1,3 +1,23 @@
+/*
+ * WINGS Operating System
+ * Copyright (C) 2016 Alyssa Rosenzweig
+ * 
+ * This file is part of WINGS.
+ * 
+ * WINGS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * WINGS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WINGS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _KSTANDARD_H
 #define _KSTANDARD_H
 
